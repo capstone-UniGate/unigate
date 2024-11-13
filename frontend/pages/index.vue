fd5f8ff8f5aa67f16398e0a79387dbbd10a4fa46
torabimohammad <moh.torabi.eng@gmail.com>
2024-11-13 16:10:08 +0100

    Create the add new group button

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,17 @@
 <template>
-    <div class="container mx-auto p-6">
-      <h1 class="text-2xl font-semibold text-gray-800 mb-6">Your Enrolled Groups</h1>
-
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        <SeeMyGroups v-for="group in groups" :key="group.id" :group="group" />
-
-      </div>
+  <div class="container mx-auto p-6">
+    <div class="flex justify-end items-center mb-6 py-2">
+    <h1 class="text-2xl font-semibold text-gray-800 mb-6">Your Enrolled Groups</h1>
+      <!-- Add the button here -->
+      <Button class="ml-auto">Create a new group</Button>
     </div>
 
-  </template>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <SeeMyGroups v-for="group in groups" :key="group.id" :group="group" />
+    </div>
+  </div>
+</template>
+
 
   <script setup>
   import SeeMyGroups from './SeeMyGroups.vue';
