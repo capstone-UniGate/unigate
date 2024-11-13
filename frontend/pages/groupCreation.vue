4efae12387bb4ae3992e9ea08c08158c62614b2d
Frazzerz <michele.frattini00@gmail.com>
2024-11-13 15:29:09 +0100

    fix: validation and tags field

--- frontend/pages/groupCreation.vue
+++ frontend/pages/groupCreation.vue
@@ -11,7 +11,7 @@
             <FormMessage />
           </FormItem>
         </FormField>
-
+  
         <!-- Course Field -->
         <FormField v-slot="{ componentField }" name="course">
           <FormItem>
@@ -27,7 +27,7 @@
             <FormMessage />
           </FormItem>
         </FormField>
-
+  
         <!-- Privacy Type (Public or Private) -->
         <div>
         <FormField v-slot="{ componentField, errorMessage }" name="isPublic">
@@ -47,7 +47,7 @@
             </FormItem>
         </FormField>
         </div>
-
+  
         <!-- Description Field -->
         <FormField v-slot="{ componentField }" name="description">
           <FormItem>
@@ -58,21 +58,64 @@
             <FormMessage />
           </FormItem>
         </FormField>
-
+  
         <!-- Tags Field -->
         <FormField v-slot="{ componentField }" name="tags">
           <FormItem>
-            <FormLabel>Tags</FormLabel>
-            <FormControl>
-              <Input type="text" placeholder="Enter tags" v-bind="componentField" />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        </FormField>
+        <FormLabel>Tags</FormLabel>
+        <FormControl>
+        <TagsInput class="px-0 gap-0 w-80" :model-value="modelValue">
+    <div class="flex gap-2 flex-wrap items-center px-3">
+     
+      <TagsInputItem v-for="item in modelValue" :key="item" :value="item">
+        <TagsInputItemText />
+        <TagsInputItemDelete />
+      </TagsInputItem>
+    </div>
 
+    <ComboboxRoot v-bind="componentField" v-model="modelValue" v-model:open="open" v-model:search-term="searchTerm" class="w-full">
+      <ComboboxAnchor as-child>
+        <ComboboxInput placeholder="Framework..." as-child>
+          <TagsInputInput class="w-full px-3" :class="modelValue.length > 0 ? 'mt-2' : ''" @keydown.enter.prevent />
+        </ComboboxInput>
+      </ComboboxAnchor>
+
+      <ComboboxPortal>
+        <ComboboxContent>
+          <CommandList
+            position="popper"
+            class="w-[--radix-popper-anchor-width] rounded-md mt-2 border bg-popover text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+            >
+            <CommandEmpty />
+            <CommandGroup>
+              <CommandItem
+                v-for="framework in filteredFrameworks" :key="framework.value" :value="framework.label"
+                @select.prevent="(ev) => {
+                  if (typeof ev.detail.value === 'string') {
+                    searchTerm = ''
+                    modelValue.push(ev.detail.value)
+                  }
+
+                  if (filteredFrameworks.length === 0) {
+                    open = false
+                  }
+                }"
+              >
+                {{ framework.label }}
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </ComboboxContent>
+      </ComboboxPortal>
+    </ComboboxRoot>
+  </TagsInput>
+</FormControl>
+</FormItem>
+<FormMessage />
+</FormField>
         <!-- Error Message Placeholder -->
         <div v-if="errorMessage" class="text-red-600">{{ errorMessage }}</div>
-
+  
         <!-- Buttons -->
         <div class="flex justify-end space-x-4 margin-bottom-custom">
           <Button type="button" @click="onCancel"  variant="destructive">
@@ -85,7 +128,7 @@
       </form>
     </div>
   </template>
-
+  
   <script setup lang="ts">
   import { Button } from '@/components/ui/button'
   import {
@@ -95,28 +138,51 @@
     FormLabel,
     FormMessage,
   } from '@/components/ui/form'
+
+  import { CommandEmpty, CommandGroup, CommandItem, CommandList } from '@/components/ui/command'
+  import { TagsInput, TagsInputInput, TagsInputItem, TagsInputItemDelete, TagsInputItemText } from '@/components/ui/tags-input'
+  import { ComboboxAnchor, ComboboxContent, ComboboxInput, ComboboxPortal, ComboboxRoot } from 'radix-vue'
+  import { computed, ref } from 'vue'
   import { Input } from '@/components/ui/input'
   import { toast } from '@/components/ui/toast'
   import { toTypedSchema } from '@vee-validate/zod'
   import { useForm } from 'vee-validate'
   import * as z from 'zod'
-
+  
   // Define the schema for validation
   const formSchema = toTypedSchema(z.object({
     name: z.string().min(2, "Name must be at least 2 characters long").max(50),
     course: z.string().nonempty("Course is required"),
     isPublic: z.enum(["public", "private"]),
-    description: z.string().optional(),
-    tags: z.string().optional(),
+    description: z.string().min(10, "The description is too short").max(300),
+    tags: z.string().min(1, "Set at least one tag"),
   }))
-
+  
   const { handleSubmit, errors } = useForm({
     validationSchema: formSchema,
   })
-
+  
   const errorMessage = ref("")
 
+  const frameworks = [
+  { value: 'next.js', label: 'Next.js' },
+  { value: 'sveltekit', label: 'SvelteKit' },
+  { value: 'nuxt', label: 'Nuxt' },
+  { value: 'remix', label: 'Remix' },
+  { value: 'astro', label: 'Astro' },
+]
+
+const modelValue = ref<string[]>([])
+const open = ref(false)
+const searchTerm = ref('')
+
+const filteredFrameworks = computed(() => frameworks.filter(i => !modelValue.value.includes(i.label)))
+  
   const onSubmit = handleSubmit((values) => {
+    if(checkTags(values.tags)){
+      errorMessage.value = "Set at least one tag"
+    }
+
     if (checkForDuplicateGroup(/*values.name, values.course*/)) {
       errorMessage.value = "Duplicate group name or already enrolled in a group for that course"
     } else {
@@ -128,20 +194,28 @@
     }
   })
 
-  const onCancel = () => {
-        history.go(-1)
+  const checkTags = (tags: string | undefined) => {
+    if(typeof tags === 'undefined'){
+      return true;
+    }
+    let actualTags = tags.split(",");
+    console.log(actualTags);
+    return actualTags.length < 1; 
 
   }
-
+  
+  const onCancel = () => {
+        history.go(-1) 
+  }
+  
   const checkForDuplicateGroup = (/*name, course*/) => {
-    // Placeholder function to check if the group already exists
     // Replace with actual check logic
     return false
   }
-
+  
   const formHasErrors = computed(() => Object.keys(errors.value).length > 0)
   </script>
-
+  
   <style scoped>
   .form-select, .form-textarea {
     width: 100%;
@@ -149,19 +223,20 @@
     border: 1px solid #ddd;
     border-radius: 0.25rem;
   }
-
+  
   .text-red-600 {
     color: #e3342f;
   }
 
   .margin-bottom-custom {
-  padding-bottom: 3px; /* o qualsiasi valore desiderato */
+  padding-bottom: 3px; 
 }
 
  .margin-top-custom {
-  padding-top: 6px; /* o qualsiasi valore desiderato */
+  padding-top: 6px; 
  }
 
 
 
   </style>
+  
