bcb66b2f0c44368d3d49eb632a84116a8f32ef48
musseGkel <musseg437@gmail.com>
2024-11-15 18:35:15 +0100

    feat: add global CSS styles and configure Tailwind CSS

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,21 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    'shadcn-nuxt',
-    '@nuxtjs/color-mode'
-  ],
+  css: ["@/assets/css/globals.css"],
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/color-mode"],
   shadcn: {
     /**
      * Prefix for all the imported component
      */
-    prefix: '',
+    prefix: "",
     /**
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: './components/ui'
-  }
-})
+    componentDir: "./components/ui",
+  },
+});
