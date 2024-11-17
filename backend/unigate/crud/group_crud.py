30402e8edb92ef1bdfe2d06f8f3355cc5abdd658
ihcsof <lorenzo.foschi.work@gmail.com>
2024-11-17 18:54:18 +0100

    feat: add endpoint for group creation; some validation missing

--- backend/unigate/crud/group_crud.py
+++ backend/unigate/crud/group_crud.py
@@ -1,4 +1,7 @@
-from sqlmodel import select
+# crud/group_crud.py
+from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError
+from sqlmodel import Session, select
 
 from unigate.models import Group
 
@@ -10,7 +13,25 @@ class CRUDGroup(CRUDBase[Group, Group, Group]):
         db_session = self.get_db()
         statement = select(self.model).where(self.model.name == name)
         result = db_session.exec(statement)
-        return result.scalar_one_or_none()
+        return result.first()
+
+    def create_group(self, *, session: Session, group_data: Group) -> Group:
+        # TODO: validate by type and not by simply name
+        if self.get_by_name(name=group_data.name):
+            raise HTTPException(
+                status_code=400, detail="Group with this name already exists."
+            )
+
+        try:
+            session.add(group_data)
+            session.commit()
+            session.refresh(group_data)
+            return group_data
+        except IntegrityError:
+            session.rollback()
+            raise HTTPException(
+                status_code=500, detail="An error occurred while creating the group."
+            )
 
 
 group_crud = CRUDGroup(Group)
