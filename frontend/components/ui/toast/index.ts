839f9d4e2f13e9a6f7fe23fd086344ce9a81b03d
torabimohammad <moh.torabi.eng@gmail.com>
2024-11-21 18:44:27 +0100

    refactor: refactor the functionality of create group button.
    fix: fix the show successful message and reroute to the main page.

--- frontend/components/ui/toast/index.ts
+++ frontend/components/ui/toast/index.ts
@@ -21,7 +21,7 @@ export const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
-        success: "bg-green-500",
+        success: "text-white bg-green-500",
       },
     },
     defaultVariants: {
