6f33612c86e5865068e0d22667a378eae19ba140
Mohammad Amin Rezaei Sepehr <mohammadaminrs1998@gmail.com>
2024-12-05 20:52:58 +0100

    Added Blocked Table

--- backend/unigate/models/__init__.py
+++ backend/unigate/models/__init__.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel  # type: ignore  # noqa: F401
 
+from unigate.models.blocked import Blocked
 from unigate.models.group import Group, GroupType
 from unigate.models.join import Join
 from unigate.models.request import Request, RequestStatus
@@ -7,6 +8,7 @@ from unigate.models.student import Student
 from unigate.models.super_student import SuperStudent
 
 __all__ = [
+    "Blocked",
     "Group",
     "GroupType",
     "Join",
