47afb9be6b6618b8c2a90d44335dc51920533943
anteris22 <g.bosi015@gmail.com>
2024-12-24 20:05:12 +0100

    fix: reject checks for pending request

--- backend/unigate/routes/group.py
+++ backend/unigate/routes/group.py
@@ -154,6 +154,11 @@ def reject_group_requesr(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="You are not a super student of this group",
         )
+    if request.status != RequestStatus.PENDING:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Request is not pending",
+        )
     return crud.group.reject_request(session=session, request=request)
 
 
