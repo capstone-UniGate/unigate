c283224787aedd9c7c27dba29134fb5238737333
fabifont <me@fabifont.dev>
2024-12-30 23:25:27 +0100

    chore: logging

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -160,6 +160,7 @@ onMounted(async () => {
   if (isLoggedIn.value) {
     await getCurrentStudent();
     if (currentStudent.value?.number) {
+      console.log(`2: ${config.public.minioURL}`);
       const currentPhotoUrl = `${config.public.minioURL}/unigate/propics/${currentStudent.value.number}`;
       eventBus.updatePhoto(currentPhotoUrl);
     }
--- frontend/components/UserPage.vue
+++ frontend/components/UserPage.vue
@@ -186,6 +186,7 @@ const isSubmitting = ref(false);
 // Remove usernameStore and create a computed photo URL
 const photoUrl = computed(() => {
   if (!currentStudent.value?.number) return null;
+  console.log(`3: ${config.public.minioURL}`);
   return `${config.public.minioURL}/unigate/propics/${currentStudent.value.number}`;
 });
 
@@ -257,6 +258,7 @@ const handleFileUpload = async (event: Event) => {
     await uploadImage(file);
     // After successful upload, update the previewUrl to the server URL
     if (currentStudent.value?.number) {
+      console.log(`1: ${config.public.minioURL}`);
       const newPhotoUrl = `${config.public.minioURL}/unigate/propics/${currentStudent.value.number}`;
       eventBus.updatePhoto(newPhotoUrl);
       previewUrl.value = newPhotoUrl;
@@ -307,6 +309,7 @@ onMounted(async () => {
 
   // Set initial preview URL
   if (currentStudent.value?.number) {
+    console.log(`4: ${config.public.minioURL}`);
     const currentPhotoUrl = `${config.public.minioURL}/unigate/propics/${currentStudent.value.number}`;
     eventBus.updatePhoto(currentPhotoUrl);
   }
