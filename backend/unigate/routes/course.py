3fce6ca12592d3b9ee9fe6e880b166bb1ce3109e
torabimohammad <moh.torabi.eng@gmail.com>
2025-01-05 17:36:15 +0100

    feat: add two endpoint for getting alive groups and distribution of creation of groups

--- backend/unigate/routes/course.py
+++ backend/unigate/routes/course.py
@@ -3,7 +3,12 @@ from fastapi import APIRouter, HTTPException, status
 from unigate import crud
 from unigate.core.database import AuthSessionDep, SessionDep
 from unigate.models import Course, Group
-from unigate.schemas.course import CourseReadWithUsersAndExams
+from unigate.schemas.course import (
+    ActiveCourseResponse,
+    CourseGroupDistributionResponse,
+    CourseReadWithUsersAndExams,
+    GroupDistributionInfo,
+)
 from unigate.schemas.group import (
     GroupReadWithStudents,
 )
@@ -71,4 +76,52 @@ def get_groups_exams(
         )
     return crud.group.get_groups_exam(
         session=session, course_name=course_name, date=date
+    )
+
+
+@router.get("/active/{course_name}", response_model=ActiveCourseResponse)
+def check_active_course(
+    session: SessionDep, course_name: str, exam_date: str
+) -> ActiveCourseResponse:
+    groups = crud.group.get_groups_exam(
+        session=session, course_name=course_name, date=exam_date
+    )
+    all_students = set()
+    student_names = []
+    for group in groups:
+        for student in group.students:
+            if student.id not in all_students:
+                all_students.add(student.id)
+                student_names.append(f"{student.name} {student.surname or ''}")
+    return ActiveCourseResponse(
+        course_name=course_name,
+        total_students=len(all_students),
+        student_names=student_names,
+    )
+
+
+@router.get(
+    "/distribution/{course_name}", response_model=CourseGroupDistributionResponse
+)
+def get_group_distribution(
+    session: SessionDep,
+    course_name: str,
+) -> CourseGroupDistributionResponse:
+    groups = crud.group.get_groups_course(session=session, course_name=course_name)
+    groups_info = [
+        GroupDistributionInfo(
+            group_name=group.name,
+            creation_date=group.date,
+            exam_date=group.exam_date,
+            creator_name=f"{group.creator.name} {group.creator.surname or ''}",
+            super_students=[
+                f"{student.name} {student.surname or ''}"
+                for student in group.super_students
+            ],
+        )
+        for group in groups
+    ]
+    groups_info.sort(key=lambda x: x.creation_date)
+    return CourseGroupDistributionResponse(
+        course_name=course_name, total_groups=len(groups_info), groups_info=groups_info
     )
