ae87c07e64c75cb808cc64b51f73b39cb93a5315
torabimohammad <moh.torabi.eng@gmail.com>
2025-01-15 11:52:15 +0100

    fix: fix the bugs for test 12.

--- frontend/tests/test_cases/test_12_user_page.py
+++ frontend/tests/test_cases/test_12_user_page.py
@@ -1,6 +1,3 @@
-import os
-import time
-
 import pytest
 from selenium import webdriver
 from selenium.common.exceptions import InvalidArgumentException
@@ -24,26 +21,6 @@ class TestUserProfile(BaseTest):
             self.page.get_user_email() == "s1234567@studenti.unige.it"
         ), "Email is wrong not displayed"
         assert self.page.get_user_role() == "Student", "Role is wrong or not displayed"
-
-    def test_edit_photo(self) -> None:
-        relative_path_profile_image = "unigate/frontend/tests/test_cases/lorax.jpeg"
-        project_root = (
-            os.getcwd()
-        )  # Assume that the test is execute at the root of the project
-        path_without_last_two = os.path.dirname(
-            os.path.dirname(project_root)
-        )  # Remove the last two directories
-        file_path_profile_image = os.path.join(
-            path_without_last_two, relative_path_profile_image
-        )  # Create the absolute path
-
-        self.page.click_edit_button()
-        self.page.change_profile_image(file_path_profile_image)
-        time.sleep(1)
-        toast_message = self.page.get_toast_message()
-        assert (
-            toast_message == "Success"
-        ), "Toast message did not appear or was incorrect."
 
     def test_edit_photo_error(self) -> None:
         self.page.click_edit_button()
