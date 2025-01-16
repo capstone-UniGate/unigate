55d96435455be550c6e2ffcb95eab1c83878aa02
fabifont <me@fabifont.dev>
2025-01-16 12:57:34 +0100

    reset prod

--- backend/unigate/main.py
+++ backend/unigate/main.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+from time import sleep
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from unigate import crud
 from unigate.core.config import settings
+from unigate.core.database import get_session
 from unigate.routes import auth, course, group, professor, student
 
 app = FastAPI()
@@ -25,6 +30,33 @@ async def hello() -> dict[str, str]:
 @app.get("/ping")
 async def ping() -> dict[str, str]:
     return {"message": "pong"}
+
+
+@app.get("/reset")
+def reset() -> dict[str, str]:
+    try:
+        with Path("/fifo").open("w") as f:
+            f.write("reset\n")
+        return {"message": "reset triggered"}
+    except Exception:
+        return {"error": "something went wrong"}
+
+@app.get("/wait")
+def wait() -> dict[str, str]:
+    sleep(5)
+    try:
+        for _ in range(120):
+            try:
+                with next(get_session()) as session:
+                    test_student = crud.student.get_by_number(number=1234567, session=session)
+                    if test_student:
+                        return {"message": "system is ready"}
+            except Exception:  # noqa: S110
+                pass
+            sleep(1)
+    except Exception:  # noqa: S110
+        pass
+    return {"error": "something went wrong"}
 
 
 app.include_router(auth.router, prefix="/auth")
