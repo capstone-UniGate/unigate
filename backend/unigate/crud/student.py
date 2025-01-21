301bf2cb9a53cf1cd5f7fc5f289164d01ee3a104
fabifont <me@fabifont.dev>
2025-01-21 21:07:48 +0100

    remove ghost code

--- backend/unigate/crud/student.py
+++ backend/unigate/crud/student.py
@@ -12,31 +12,5 @@ class CRUDStudent(CRUDBase[Student, StudentCreate, Student]):
         result = session.exec(statement)
         return result.one_or_none()
 
-    # def get_members(
-    #     self, *, group_id: uuid.UUID, student_id: uuid.UUID | None
-    # ) -> list[Student]:
-    #     group = group.get(id=group_id)
-    #     if group.type == GroupType.PRIVATE and not self.check_membership(
-    #         group_id=group_id, student_id=student_id
-    #     ):
-    #         return []
-    #     statement = select(self.model).join(Join).where(Join.group_id == group_id)
-    #     return self.get_multi(query=statement)
-
-    # def check_membership(
-    #     self, *, group_id: uuid.UUID, student_id: uuid.UUID | None
-    # ) -> bool:
-    #     if student_id is None:
-    #         return False
-    #     db_session = self.get_db_session()
-    #     statement = (
-    #         select(self.model)
-    #         .join(Join)
-    #         .where(Group.id == group_id)
-    #         .where(self.model.id == student_id)
-    #     )
-    #     result = db_session.exec(statement)
-    #     return result.one_or_none() is not None
-
 
 student = CRUDStudent(Student)
